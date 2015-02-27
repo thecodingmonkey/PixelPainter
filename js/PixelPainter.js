793c0c89affa75b9a4375e32f919a57bca544dae
romanarellano <romanarellano@yahoo.com>
2015-02-26 20:06:57 -1000

    added grid color

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,88 +1,91 @@
-$(document).ready(function() {
+$(document).ready(function() 
+{
 //  alert('hello');
+    var currentColor = "#000";
 
-  var pixelSize = 20;
-  var currentColor = "#000";
-  var dragMode = false;
-  var colors = ["#000", "#fff"];
-//  var pixelPainter = null;
+function PixelPainter(width,height){
 
-  function PixelPainter(height, width) {
-    var result = {};
 
-    result.controls = $("<div>");
-    result.controls.width(200);
-    result.controls.height(200);
+  var Paint = { 
 
-    result.artboard = $("<div>");
+    controls: $("<div>"),
+    artboard: $("<div>")
 
-    // init controls
-    var swatch = $("<div>");
+  };
 
-    colors.map( function(val) {
-      var swatchColor = $("<div>");
-      swatchColor.css("border", "1px black solid");
-      swatchColor.width(pixelSize + "px");
-      swatchColor.height(pixelSize + "px");
-      swatchColor.css("display", "inline-block");
-      swatchColor.css("background-color", val);
-      swatchColor.click(function() {
-        val.css("border", "1px black solid");
+  Paint.artboard.width(200);
+  Paint.artboard.height(200);
 
-        currentColor = $(this).css("background-color");
-//        $(this).css('border', "1px black gray");
-//        console.log(currentColor);
-      });
 
-      swatch.append(swatchColor);  
-    });
+  for(var i=0;i< width*height;i++){
 
-    result.controls.append(swatch);
+
+    var pixel = $("<div>");
+    pixel.width(width + "px");
+    pixel.height(height + "px");
+    pixel.css("border","solid 1px #000");
+    pixel.css("display","inline-block");
+
+    pixel.on("mousedown",mouseDown);
+    pixel.on("mouseenter",mouseDrag);
+    pixel.on("mouseup",mouseUp);
 
 
 
-    // init canvas    
+    Paint.artboard.append(pixel);
 
-    // add 2 to width because of the border
-    result.artboard.width(pixelSize * (2+width) );
-
-    var i;
-
-    var fillColorStart = function() {
-      $(this).css("background-color", currentColor);
-      dragMode = true;
-    };
-
-    var fillColorDrag = function() {
-      if (dragMode) {
-        $(this).css("background-color", currentColor);
-      }
-    };
-
-    var fillColorStop = function() {
-      dragMode = false;
-    };
-
-
-    for(i=0; i < height*width; i++) {
-      var pixel = $("<div>");
-      pixel.css("border", "1px black solid");
-      pixel.width(pixelSize + "px");
-      pixel.height(pixelSize + "px");
-      pixel.css("display", "inline-block");
-
-      pixel.on("mousedown", fillColorStart);
-      pixel.on("mouseenter", fillColorDrag);
-      pixel.on("mouseup", fillColorStop);
-
-      result.artboard.append(pixel);
-    }
-
-    return result;
   }
 
+  function mouseDown(){
 
-  var pixelPainter = PixelPainter(20,20);
-  $("#controls").append(pixelPainter.controls);
-  $("#artboard").append(pixelPainter.artboard);
+
+    $(this).css("background-color",currentColor);
+    dragMode = true;
+  }
+
+  function mouseUp(){
+
+    $(this).css("background-color", currentColor);
+    dragMode = false;
+
+  }
+
+  function mouseDrag(){
+
+    if(dragMode){
+
+      $(this).css("background-color",currentColor);
+    }
+  } 
+
+  // var colors = ["#000","#FFF"];
+
+  // colors.map(function(val){
+
+  //   var color = $("div");
+
+  //   color.width(width + "px");
+  //   color.height(height + "px");
+  //   color.css("background-color", val);
+  //   color.css("border","1px solid black");
+  //   color.css("display","inline-block");
+
+  //   var currentColor = $(this).css("background");
+
+  //   Paint.controls.append(color);
+  // });
+
+  
+
+  return Paint;
+
+}
+
+
+
+
+
+var pixelPainter = PixelPainter(20,20);
+$("#controls").append(pixelPainter.controls);
+$("#artboard").append(pixelPainter.artboard);
 });
